44ef7d748031eda85f0b1ae05039bbed11667a85
Samuel Rafino <samucarafino@gmail.com>
2026-08-10 17:44:03 -0300

    feat: adicionar rotas de cadastro de perguntas, e configuracao do jogo

---
+++ app/cadastro-perguntas/page.tsx
@@ -0,0 +1,4 @@
+export default function CadastroPerguntasPage() {
+  // este é o template da página de login, você pode personalizá-lo como quiser, basta alterar o conteúdo do return abaixo dentro dos parenteses, exemplo: "return(<div> <h1>Este é um exemplo </h1> </div>)". Lembre-se de manter a estrutura do Next.js, com a função export default e o nome da página e aberturas e fechamentos de chaves corretos.
+  return <h1>Coloque aqui seu template</h1>;
+}
